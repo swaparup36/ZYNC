3854ebb78f9cee6fa86033bd48eebbda4e5195af
swaparup36 <swaparup36@gmail.com>
2026-02-06 14:24:49 +0530

    fix: favicon updated

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -14,19 +14,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/favicon.ico',
+        type: 'image/x-icon',
       },
     ],
-    apple: '/apple-icon.png',
   },
 }
 
